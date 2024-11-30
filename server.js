8105e6e43629f161fdb5a6141dc7b89eb75be716
Aarti1113 <aartikashyap9097@gmail.com>
2024-12-01 00:03:14 +0530

    updated port related stuff

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ mongoose
   .catch((err) => console.log("Error connecting to MongoDB:", err));
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
